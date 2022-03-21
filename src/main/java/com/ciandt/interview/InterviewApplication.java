21f69cdf188829ce55272bf522e4f62be9cf59ea
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2022-03-21 13:46:34 -0300

    Adding mvc dependency and simple controller

--- src/main/java/com/ciandt/interview/InterviewApplication.java
+++ src/main/java/com/ciandt/interview/InterviewApplication.java
@@ -2,8 +2,12 @@ package com.ciandt.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ComponentScan("com.ciandt")
+@EnableWebMvc
 public class InterviewApplication {
 
 	public static void main(String[] args) {
