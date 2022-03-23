ac9bb14449fff6795439439f18ffab86e8f7b202
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2022-03-23 16:33:20 -0300

    Removing spring annotation

--- src/main/java/com/ciandt/interview/InterviewApplication.java
+++ src/main/java/com/ciandt/interview/InterviewApplication.java
@@ -1,13 +1,8 @@
 package com.ciandt.interview;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class InterviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InterviewApplication.class, args);
 	}
 
 }
