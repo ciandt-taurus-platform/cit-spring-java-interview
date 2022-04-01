a9a14b1bdbc45f8e7cf38a509bca7b7290fb8fb3
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2022-04-01 16:18:47 -0300

    Adding new test scenarios

--- src/main/java/com/ciandt/interview/InterviewApplication.java
+++ src/main/java/com/ciandt/interview/InterviewApplication.java
@@ -5,4 +5,7 @@ public class InterviewApplication {
 	public static void main(String[] args) {
 	}
 
+	public static String generateShortUrl(String inputUrl) {
+		return null;
+	}
 }
--- src/test/java/com/ciandt/interview/InterviewApplicationTests.java
+++ src/test/java/com/ciandt/interview/InterviewApplicationTests.java
@@ -1,13 +1,51 @@
 package com.ciandt.interview;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-@SpringBootTest
 class InterviewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @ParameterizedTest
+  @CsvSource({
+      "https://ciandt.com, https://shorter.url/abcef",
+      "https://ciandt.com/en, https://shorter.url/abceg",
+      "https://ciandt.com/pt, https://shorter.url/abceh",
+      "https://linkedin.com, https://shorter.url/abc12f",
+      "https://msn.com, https://shorter.url/c7e78",
+  })
+  void shouldShortToFiveChars(String inputUrl, String shorterUrl) {
+    var shortUrl = InterviewApplication.generateShortUrl(inputUrl);
+    Assertions.assertThat(shortUrl).isEqualTo(shorterUrl);
+  }
+
+//
+//  @ParameterizedTest
+//  @CsvSource({
+//      "https://ciandt.com, https://shorter.url/db77c530",
+//      "https://ciandt.com/en, https://shorter.url/db77c530",
+//      "https://ciandt.com/pt, https://shorter.url/c33f30ca",
+//      "https://linkedin.com, https://shorter.url/f1da69f2",
+//  })
+//  void shouldShortToParameterizeLenghtChars(String inputUrl, String shorterUrl) {
+//    var shortUrl = InterviewApplication.generateShortUrl(inputUrl, 8);
+//    Assertions.assertThat(shortUrl).isEqualTo(shorterUrl);
+//  }
+
+
+//  @ParameterizedTest
+//  @CsvSource({
+//      "https://ciandt.com, https://shorter.url/db77c530",
+//      "https://ciandt.com/en, https://shorter.url/db77c530",
+//      "https://ciandt.com/pt, https://shorter.url/c33f30ca",
+//      "https://linkedin.com, https://shorter.url/f1da69f2",
+//  })
+//  void shouldBeAbleToReturnToOriginalUrl(String inputUrl, String shorterUrl) {
+//    var shortUrl = InterviewApplication.generateShortUrl(inputUrl, 8);
+//    Assertions.assertThat(shortUrl).isEqualTo(shorterUrl);
+//
+//    var original = InterviewApplication.getOriginalUrl(shortUrl);
+//    Assertions.assertThat(original).isEqualTo(inputUrl);
+//  }
 
 }
